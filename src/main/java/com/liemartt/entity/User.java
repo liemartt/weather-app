206620b8da1699cab0e6fdfc6658faf15420c1e9
Artem_Limonov <liemartt@yandex.ru>
2024-08-06 21:25:26 +0300

    chore: remove email from user entity

--- src/main/java/com/liemartt/entity/User.java
+++ src/main/java/com/liemartt/entity/User.java
@@ -21,9 +21,6 @@ public class User {
     @Column(unique = true)
     private String username;
     
-    @Column(unique = true)
-    private String email;
-    
     private String password;
     
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
