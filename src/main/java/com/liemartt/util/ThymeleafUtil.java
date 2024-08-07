78b31e4c0c700594dfe5386ac02acaf2a9023f5b
Artem_Limonov <liemartt@yandex.ru>
2024-08-07 18:31:34 +0300

    feat: add ThymeleafUtil

---
+++ src/main/java/com/liemartt/util/ThymeleafUtil.java
@@ -0,0 +1,45 @@
+package com.liemartt.util;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+@WebListener
+public class ThymeleafUtil implements ServletContextListener {
+    private static final TemplateEngine engine = templateEngine();
+    
+    public static WebContext getWebContext(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext) {
+        IWebExchange iWebExchange = JakartaServletWebApplication.buildApplication(servletContext)
+                .buildExchange(req, resp);
+        return new WebContext(iWebExchange);
+    }
+    
+    public static void process(WebContext context, String templateName) {
+        engine.process(templateName, context);
+    }
+    
+    private static TemplateEngine templateEngine() {
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+    
+    private static ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+    
+    
+}
