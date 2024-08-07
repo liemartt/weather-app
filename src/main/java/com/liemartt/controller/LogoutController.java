91bb9b83c98bb5290e5bb4291e7d95383f386288
Artem_Limonov <liemartt@yandex.ru>
2024-08-07 11:36:36 +0300

    feat: create base structure of controllers

---
+++ src/main/java/com/liemartt/controller/LogoutController.java
@@ -0,0 +1,17 @@
+package com.liemartt.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    
+    }
+}
