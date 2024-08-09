0b0be7b64da904c5e73f03dee5c5f17fce95b44c
Artem_Limonov <liemartt@yandex.ru>
2024-08-09 19:37:55 +0300

    feat: add scheduler to delete expired sessions

--- src/main/java/com/liemartt/dao/SessionDAO.java
+++ src/main/java/com/liemartt/dao/SessionDAO.java
@@ -3,16 +3,14 @@ package com.liemartt.dao;
 import com.liemartt.entity.Session;
 import com.liemartt.entity.User;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface SessionDAO {
     Session createSession(User user);
     
     void endSession(Session session);
     
-    List<Session> getAllExpiredSessions();
+    void endAllExpiredSessions();
     
     Optional<Session> getSessionByUUID(String sessionUUID);
 }
