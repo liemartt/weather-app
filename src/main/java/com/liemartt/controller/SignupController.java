1e1fa66a74729b4da940f0609585b5516c270072
Artem_Limonov <liemartt@yandex.ru>
2024-08-09 15:26:06 +0300

    chore: rename

---
+++ src/main/java/com/liemartt/controller/SignupController.java
@@ -0,0 +1,42 @@
+package com.liemartt.controller;
+
+import com.liemartt.dto.SignupUserDto;
+import com.liemartt.exception.UsernameAlreadyExistsException;
+import com.liemartt.service.SignupService;
+import com.liemartt.util.ThymeleafUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.WebContext;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/signup")
+public class SignupController extends HttpServlet {
+    private final SignupService signupService = SignupService.getInstance();
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = ThymeleafUtil.getWebContext(req, resp, getServletContext());
+        ThymeleafUtil.process(context, "signup.html", resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        WebContext context = ThymeleafUtil.getWebContext(req, resp, getServletContext());
+        
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        SignupUserDto userDto = new SignupUserDto(username, password);
+        try {
+            signupService.signupNewUser(userDto);
+        } catch (UsernameAlreadyExistsException e) {
+            context.setVariable("error", e.getMessage());//TODO responseEntity?
+            ThymeleafUtil.process(context, "signup.html", resp);
+            return;
+        }
+    }
+}
