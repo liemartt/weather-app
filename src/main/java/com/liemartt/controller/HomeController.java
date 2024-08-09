b5a20c44cc1484be29bed1add0c111c4246cec2f
Artem_Limonov <liemartt@yandex.ru>
2024-08-09 19:49:48 +0300

    feat: add authorization validation

--- src/main/java/com/liemartt/controller/HomeController.java
+++ src/main/java/com/liemartt/controller/HomeController.java
@@ -1,20 +1,35 @@
 package com.liemartt.controller;
 
+import com.liemartt.entity.User;
+import com.liemartt.exception.UserNotAuthorizedException;
+import com.liemartt.service.SessionService;
+import com.liemartt.util.ThymeleafUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/")
 public class HomeController extends HttpServlet {
+    private final SessionService sessionService = SessionService.getINSTANCE();
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    
+        WebContext context = ThymeleafUtil.getWebContext(req, resp, getServletContext());
+        try {
+            User user = sessionService.getAuthorizedUser(req.getCookies());
+        } catch (UserNotAuthorizedException e) {
+            context.setVariable("error", "Please log in or sign up");
+            ThymeleafUtil.process(context, "index.html", resp);
+            return;
+        }
+        
     }
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     
--- src/main/java/com/liemartt/service/SessionService.java
+++ src/main/java/com/liemartt/service/SessionService.java
@@ -2,8 +2,14 @@ package com.liemartt.service;
 
 import com.liemartt.dao.SessionDAO;
 import com.liemartt.dao.SessionDAOImpl;
+import com.liemartt.entity.Session;
+import com.liemartt.entity.User;
+import com.liemartt.exception.UserNotAuthorizedException;
+import jakarta.servlet.http.Cookie;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -16,21 +22,23 @@ public class SessionService {
     
     private SessionService() {
     }
+    
     public void scheduleSessionDeletion() {
-            scheduler.scheduleAtFixedRate(sessionDAO::endAllExpiredSessions, 0, 5, TimeUnit.MINUTES);
-        }
+        scheduler.scheduleAtFixedRate(sessionDAO::endAllExpiredSessions, 0, 5, TimeUnit.MINUTES);
     }
-    //    public Optional<User> getAuthorizedUser(Cookie[] cookies){
-//        Optional<Cookie> sessionCookie = Arrays.stream(cookies)
-//                .filter(cookie -> cookie.getName().equals("sessionId"))
-//                .findFirst();
-//        if (sessionCookie.isPresent()){
-//            String sessionId = sessionCookie.get().getValue();
-//            Optional<Session> session = sessionDAO.getSessionByUUID(sessionId);
-//            return session.ifPresentOrElse(userSession->userSession.getUser(), ()->Optional.empty());
-//            if(session.isPresent()){
-//                return Optional.ofNullable(session.get().getUser());
-//            }
-//        }
-//    }
+    
+    public User getAuthorizedUser(Cookie[] cookies) {
+        Optional<Cookie> sessionCookie = Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("sessionId"))
+                .findFirst();
+        if (sessionCookie.isPresent()) {
+            String sessionId = sessionCookie.get().getValue();
+            Optional<Session> session = sessionDAO.getSessionByUUID(sessionId);
+            if (session.isPresent()) {
+                return session.get().getUser();
+            }
+        }
+        throw new UserNotAuthorizedException();
+    }
+}
 
