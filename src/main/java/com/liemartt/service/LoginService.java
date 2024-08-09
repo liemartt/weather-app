fa6910c67f5e78a37d150fdee5124251d36d3fb1
Artem_Limonov <liemartt@yandex.ru>
2024-08-09 17:19:41 +0300

    feat: implemented LoginService & SignupService

---
+++ src/main/java/com/liemartt/service/LoginService.java
@@ -0,0 +1,39 @@
+package com.liemartt.service;
+
+import com.liemartt.dao.SessionDAO;
+import com.liemartt.dao.SessionDAOImpl;
+import com.liemartt.dao.UserDAO;
+import com.liemartt.dao.UserDAOImpl;
+import com.liemartt.dto.UserDto;
+import com.liemartt.entity.Session;
+import com.liemartt.entity.User;
+import com.liemartt.exception.IncorrectPasswordException;
+import com.liemartt.exception.UserNotFoundException;
+import lombok.Getter;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class LoginService {
+    @Getter
+    private final static LoginService INSTANCE = new LoginService();
+    private final SessionDAO sessionDAO = new SessionDAOImpl();
+    private final UserDAO userDAO = new UserDAOImpl();
+    
+    private LoginService() {
+    }
+    
+    public void validateUser(UserDto userDto) throws UserNotFoundException, IncorrectPasswordException {
+        Optional<User> userOptional = userDAO.getUserByUsername(userDto.getUsername());
+        if (!BCrypt.checkpw(userDto.getPassword(), userOptional.get().getPassword())) {
+            throw new IncorrectPasswordException();
+        }
+    }
+    
+    
+    public boolean isSessionValid(UUID sessionUUID) {//todo move in SessionService?
+        Optional<Session> session = sessionDAO.getSessionByUUID(sessionUUID);
+        return session.isPresent();
+    }
+}
---
+++ src/main/java/com/liemartt/service/SignupService.java
@@ -0,0 +1,24 @@
+package com.liemartt.service;
+
+import com.liemartt.dao.UserDAO;
+import com.liemartt.dao.UserDAOImpl;
+import com.liemartt.dto.UserDto;
+import com.liemartt.entity.User;
+import com.liemartt.exception.UsernameAlreadyExistsException;
+import lombok.Getter;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class SignupService {
+    @Getter
+    private final static SignupService INSTANCE = new SignupService();
+    private final UserDAO userDAO = new UserDAOImpl();
+    
+    private SignupService() {
+    }
+    
+    public void signupNewUser(UserDto userDto) throws UsernameAlreadyExistsException {
+        String hashedPassword = BCrypt.hashpw(userDto.getPassword(), BCrypt.gensalt());
+        User user = new User(userDto.getUsername(), hashedPassword);
+        userDAO.saveUser(user);
+    }
+}
