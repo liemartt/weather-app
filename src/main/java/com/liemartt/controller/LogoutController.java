4c482a73007c8548a15df8bc4556466665bf4449
Artem_Limonov <liemartt@yandex.ru>
2024-08-10 20:58:15 +0300

    chore: update

--- src/main/java/com/liemartt/controller/LogoutController.java
+++ src/main/java/com/liemartt/controller/LogoutController.java
@@ -1,23 +1,36 @@
 package com.liemartt.controller;
 
-import com.liemartt.service.LogoutService;
-import com.liemartt.util.ThymeleafUtil;
+import com.liemartt.dao.SessionDAO;
+import com.liemartt.dao.SessionDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
 
 @WebServlet(urlPatterns = "/logout")
 public class LogoutController extends HttpServlet {
-    private final LogoutService logoutService = LogoutService.getINSTANCE();
+    private final SessionDAO sessionDAO = new SessionDAOImpl();
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logoutService.logout(req.getCookies());
+        Optional<Cookie> sessionCookie = Arrays.stream(req.getCookies())
+                .filter(cookie -> cookie.getName().equals("sessionId"))
+                .findFirst();
+        String sessionId = sessionCookie.get().getValue();
+        
+        UUID sessionUUID = UUID.fromString(sessionId);
+        sessionDAO.endSession(sessionUUID);
+        
+        sessionCookie.get().setMaxAge(0);
+        resp.addCookie(sessionCookie.get());
+        
         resp.sendRedirect(req.getContextPath() + "/");
     }
 }
